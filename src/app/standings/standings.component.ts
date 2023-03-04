efb155353c82f1e27e58605609a1632fdf195aad
Ricardo Figueroa <rfigueroa2317@gmail.com>
2023-03-03 23:41:55 -0500

    adding the standing table

--- src/app/standings/standings.component.ts
+++ src/app/standings/standings.component.ts
@@ -10,11 +10,27 @@ import { StandingService } from '../service/standing.service';
 export class StandingsComponent implements OnInit {
   selectedStanding?: IStandings;
 
-  standings: IStandings[] = [];
+  // id: number;
+  //   team: string;
+  //   win: number;
+  //   loss: number;
+  //   draw: number;
+
+  STANDINGS_DATA: IStandings[] = [
+    {id: 1, team: 'one', win: 0, loss: 0, draw: 0},
+    {id: 2, team: 'two', win: 0, loss: 0, draw: 0},
+    {id: 3, team: 'three', win: 0, loss: 0, draw: 0},
+    {id: 4, team: 'four', win: 0, loss: 0, draw: 0},
+    {id: 5, team: 'five', win: 0, loss: 0, draw: 0}
+  ];
 
   constructor(private standingService: StandingService) {}
 
   ngOnInit(): void {
 
   }
+
+  displayedColums: string[] = ['id','team','win','loss','draw'];
+  dataSource = this.STANDINGS_DATA;
+
 }
