472d68facdaf5c2db4609b5443de89005a3e1259
Ricardo Figueroa <rfigueroa2317@gmail.com>
2023-04-07 15:22:03 -0400

    adding teams to standings

--- src/app/standings/standings.component.ts
+++ src/app/standings/standings.component.ts
@@ -17,11 +17,14 @@ export class StandingsComponent implements OnInit {
   //   draw: number;
 
   STANDINGS_DATA: IStandings[] = [
-    { id: 1, team: 'one', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
-    { id: 2, team: 'two', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
-    { id: 3, team: 'three', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
-    { id: 4, team: 'four', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
-    { id: 5, team: 'five', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 }
+    { id: 1, team: 'Brawns', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 2, team: 'Foresters', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 3, team: 'Nightmares', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 4, team: 'Ice Caps', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 5, team: 'Flames', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 6, team: 'Dragons', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 7, team: 'Brainiacs', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
+    { id: 8, team: 'Salty Crew', win: 0, loss: 0, draw: 0, pf: 0, pa: 0 },
   ];
 
   constructor(private standingService: StandingService) { }
